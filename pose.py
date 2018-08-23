b141c7e8ec70da75b8c863b597833851795e812f
Matteo Fabbri <194174@studenti.unimore.it>
2018-08-23 15:44:36 +0200

    Update pose.py

--- pose.py
+++ pose.py
@@ -61,7 +61,8 @@ class Pose(list):
 			joint_a = self[j_id_a] # type: Joint
 			joint_b = self[j_id_b] # type: Joint
 			t = 1 if joint_a.cam_distance > 25 else 2
-			cv2.line(image, joint_a.pos2d, joint_b.pos2d, color=color, thickness=t)
+			if joint_a.is_on_screen and joint_b.is_on_screen:
+				cv2.line(image, joint_a.pos2d, joint_b.pos2d, color=color, thickness=t)
 
 		# draw joint(s) circles
 		for joint in self:
